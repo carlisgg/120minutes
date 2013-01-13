76d6099721bfe0de259e7b60ff37bfb3cd2294f1
Luis <luyx256@gmail.com>
2013-01-13 11:30:05 +0100

    pestañas bookmarkables

--- public/javascripts/120m-functions.js
+++ public/javascripts/120m-functions.js
@@ -1,6 +1,24 @@
 $(document).ready(function() {
 
-	$("#add-tema-puedo").click({
+	// Function to activate the tab
+	function activateTab() {
+		var activeTab = $('[href=' + window.location.hash.replace('/', '') + ']');
+		activeTab && activeTab.tab('show');
+	}
+	
+	// Trigger when the hash changes (forward / back)
+	$(window).bind('hashchange', function(){
+		activateTab();
+	});
+	
+	activateTab();
+	
+	// Change hash when a tab changes
+    $('a[data-toggle="tab"], a[data-toggle="pill"]').on('shown', function(){
+        window.location.hash = $(this).attr('href').replace('#', '');
+    });
+	
+	$("#add-tema-puedo").click(function() {
 		alert("jo");
 	});
 
